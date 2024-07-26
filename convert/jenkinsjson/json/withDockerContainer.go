a17da817985e287baccd6c43b85ff345096ba8a7
HarnessAryan <aryan.garg@harness.io>
2024-07-26 20:02:11 +0530

    Update withDockerContainer.go

--- convert/jenkinsjson/json/withDockerContainer.go
+++ convert/jenkinsjson/json/withDockerContainer.go
@@ -3,17 +3,16 @@ package json
 import (
 	"encoding/json"
 	"log"
-
 	harness "github.com/drone/spec/dist/go"
 )
 
 func ConvertWithDockerContainer(node Node, variables map[string]string) *harness.Step {
-	var script, image string
+	var args, image string
 	if attr, ok := node.AttributesMap["harness-attribute"]; ok {
 		var attrMap map[string]interface{}
 		if err := json.Unmarshal([]byte(attr), &attrMap); err == nil {
-			if s, ok := attrMap["script"].(string); ok {
-				script = s
+			if a, ok := attrMap["args"].(string); ok {
+				args = a
 			}
 			if i, ok := attrMap["image"].(string); ok {
 				image = i
@@ -26,13 +25,12 @@ func ConvertWithDockerContainer(node Node, variables map[string]string) *harness
 		return nil
 	}
 
-	if script == "" {
-		log.Printf("no script found for node %s", node.SpanName)
-		return nil
+	if args == "" {
+		log.Printf("no args found for node %s", node.SpanName)
 	}
 
 	if image == "" {
-		image = "default-image:latest"
+		image = "default-image:latest" 
 	}
 
 	step := &harness.Step{
@@ -41,7 +39,7 @@ func ConvertWithDockerContainer(node Node, variables map[string]string) *harness
 		Type: "Run",
 		Spec: &harness.StepExec{
 			Image: image,
-			Run:   script,
+			Run:   args,
 		},
 	}
 
